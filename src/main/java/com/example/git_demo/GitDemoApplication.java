8c41a897d1eae5f09acad92bd1235a1c645af2cd
ikaa96 <slavicivana96@gmail.com>
2024-11-15 13:34:41 +0100

    mvc arh

--- src/main/java/com/example/git_demo/GitDemoApplication.java
+++ src/main/java/com/example/git_demo/GitDemoApplication.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
 
 
 }
